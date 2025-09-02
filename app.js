581628dacdadf683e242d2eee5c4c1d92c41a599
Pradhan <ppradhan2214@gmail.com>
2025-09-02 23:36:15 +0530

    Add additional files and minor updates

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.get("/listings/new", (req, res) => {
 app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing =   await Listing.findById(id);
-    console.log("Listing image: ", listing.image);
     res.render("listings/show.ejs", { listing });
 });
 
