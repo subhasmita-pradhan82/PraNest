de1f592a98ec34f7380bc955b1805a6ef82b7261
Pradhan <ppradhan2214@gmail.com>
2025-09-28 13:04:16 +0530

    Fix app.js

--- app.js
+++ app.js
@@ -120,6 +120,11 @@ app.use((err, req, res, next) => {
 
 
 
-app.listen(8080, () => {
-    console.log("Server is working to port 8080");
+// app.listen(8080, () => {
+//     console.log("Server is working to port 8080");
+// });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
